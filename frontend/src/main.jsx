1ef66c25f4843e95c813e0da22573fc6ddec94e2
Jakaria <jakariait@gmail.com>
2025-05-18 00:56:51 +0600

    GTM Added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,12 @@ import "./index.css";
 import "quill/dist/quill.snow.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import App from "./App.jsx";
+import TagManager from "react-gtm-module";
 
+const tagManagerArgs = {
+  gtmId: "GTM-KS63HB6S", // replace with your ID
+};
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
