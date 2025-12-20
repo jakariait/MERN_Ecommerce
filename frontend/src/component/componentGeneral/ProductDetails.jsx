7dde33d85e3040a0502dbf3889f1b02a79131f02
Jakaria <jakariait@gmail.com>
2025-12-20 21:20:31 +0600

    Update

--- frontend/src/component/componentGeneral/ProductDetails.jsx
+++ frontend/src/component/componentGeneral/ProductDetails.jsx
@@ -145,7 +145,6 @@ const ProductDetails = () => {
     setRecentlyViewed(viewed);
   }, [product]);
 
-  // If product is loading, show a loading screen
   if (loading || product?.slug !== slug) {
     return (
       <div className="xl:container xl:mx-auto p-3">
@@ -169,13 +168,39 @@ const ProductDetails = () => {
               <Skeleton height={50} width={"90%"} />
             </div>
             <Skeleton height={50} width={"90%"} />
-            
+
             {/* Skeleton for Social Share, Product Code, Short Desc */}
             <div className="flex flex-col gap-3 pt-4">
               <Skeleton height={24} width={"60%"} />
               <Skeleton height={20} width={"40%"} />
               <Skeleton count={2} height={18} width={"90%"} />
             </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 pt-4">
+          <Skeleton height={400} width={"100%"} />
+          <Skeleton count={2} height={60} width={"100%"} />
+        </div>
+        <div className="flex flex-col gap-3 pt-4">
+          <Skeleton height={40} width={"100%"} />
+          <div className={"grid grid-cols-4 gap-4"}>
+            <Skeleton height={300} width={"100%"} />
+            <Skeleton height={300} width={"100%"} />
+            <Skeleton height={300} width={"100%"} />
+            <Skeleton height={300} width={"100%"} />
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 pt-4">
+          <Skeleton height={40} width={"100%"} />
+          <div className={"grid grid-cols-4 gap-4"}>
+            <Skeleton height={300} width={"100%"} />
+            <Skeleton height={300} width={"100%"} />
+            <Skeleton height={300} width={"100%"} />
+            <Skeleton height={300} width={"100%"} />
+            <Skeleton height={300} width={"100%"} />
+            <Skeleton height={300} width={"100%"} />
+            <Skeleton height={300} width={"100%"} />
+            <Skeleton height={300} width={"100%"} />
           </div>
         </div>
       </div>
