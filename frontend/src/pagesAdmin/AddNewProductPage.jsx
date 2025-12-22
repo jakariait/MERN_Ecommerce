ee37a20e3f9f7020caa5374d33ca2176b3d094a5
Jakaria <jakariait@gmail.com>
2025-12-22 20:50:22 +0600

    Product Form Added

--- frontend/src/pagesAdmin/AddNewProductPage.jsx
+++ frontend/src/pagesAdmin/AddNewProductPage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import LayoutAdmin from "../component/componentAdmin/LayoutAdmin.jsx";
 import Breadcrumb from "../component/componentAdmin/Breadcrumb.jsx";
-import AddProduct from "../component/componentAdmin/AddProduct.jsx";
 import RequirePermission from "../component/componentAdmin/RequirePermission.jsx";
+import ProductForm from "../component/componentAdmin/ProductForm.jsx";
 
 const AddNewProductPage = () => {
   return (
     <LayoutAdmin>
       <Breadcrumb pageDetails="PRODUCT" title="Add New Product" />
       <RequirePermission permission="add_products">
-        <AddProduct />
+        <ProductForm isEdit={false} />
       </RequirePermission>
     </LayoutAdmin>
   );
--- frontend/src/pagesAdmin/EditProductPage.jsx
+++ frontend/src/pagesAdmin/EditProductPage.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import LayoutAdmin from "../component/componentAdmin/LayoutAdmin.jsx";
 import Breadcrumb from "../component/componentAdmin/Breadcrumb.jsx";
-import UpdateProduct from "../component/componentAdmin/UpdateProduct.jsx";
 import RequirePermission from "../component/componentAdmin/RequirePermission.jsx";
-import ViewAllProducts from "../component/componentAdmin/ViewAllProducts.jsx";
+import ProductForm from "../component/componentAdmin/ProductForm.jsx";
 
 const EditProductPage = () => {
   return (
     <LayoutAdmin>
       <Breadcrumb pageDetails="PRODUCT" title="Edit Product" />
       <RequirePermission permission="edit_products">
-        <UpdateProduct />
+        <ProductForm isEdit={true} />
       </RequirePermission>
     </LayoutAdmin>
   );
