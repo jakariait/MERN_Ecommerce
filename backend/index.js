6ae20182afbc76a3c38587b94822b8cb84d87ab3
Jakaria <jakariait@gmail.com>
2025-12-23 19:10:54 +0600

    Admin Loading Added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
